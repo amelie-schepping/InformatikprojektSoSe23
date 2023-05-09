069b4a7793fdc8c2d0f9e86a7cc4bfeca25fd09d
amelieschepping <amelie.schepping@gmail.com>
2023-05-09 18:24:52 +0200

    erstes OOP Design für MNK-Spiel

---
+++ main.py
@@ -0,0 +1,46 @@
+class Board:
+    m = 5
+    n = 5
+    k = 4
+
+    def initialize(_m, _n):
+        m = _m
+        n = _n
+
+    def reset():
+
+
+    def display():
+
+
+    def has_won():
+        return 0
+class Player:
+    name = ""
+    player_number = 0 # 1 or 2
+    symbol = "" # x or o
+
+    def make_move(_board):
+        row = 0
+        col = 0
+
+class MyBot(Player):
+    def make_move(_board):
+        row = 0
+        col = 0
+
+class Game:
+    m = 5
+    n = 5
+    k = 4
+
+    Board
+    Player
+
+    def start():
+
+    def initialize():
+
+
+
+    def game_loop():
