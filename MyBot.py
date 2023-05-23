fd6bef6451beb5eee4d4d1e963ea2f4660bb1531
amelieschepping <amelie.schepping@gmail.com>
2023-05-23 17:38:42 +0200

    - make_move in MyBot: zufälliger Spielzug
    Überlegung: zu Beginn des Spiels sollte eine Abfrage erfolgen, in welchem Modus gespielt werden möchte: Mensch vs Mensch, Mensch vs Bot (einfach), Mensch vs Bot (schwer)

--- MyBot.py
+++ MyBot.py
@@ -1,14 +1,37 @@
 from Player import Player
+import numpy as np
 
 """
 Die Klasse MyBot ist eine Subklasse von der Klasse Player
 """
+
+
 class MyBot(Player):
+    """
+    Konstruktor der Klasse Player
+    - initialisiert folgende Instanzvariablen
+    name: Name von Player
+    player_number: Player 1 oder 2
+    symbol: Symbol des Players für die Spielzüge
+    """
+
+    def __init__(self, player_number, symbol):
+        self.name = "MyBot"
+        self.player_number = player_number
+        self.symbol = symbol
 
     """
     Funktion für den Spielzug des Bots
     """
-    def make_move(self, _board):
-        self.row = 0
-        self.col = 0
 
+    def make_move(self, board):
+
+        # generiert so lange Zufallszahlen, bis eine gültige gefunden wurde
+        while True:
+            # random.randint erzeugt Zufallszahlen von 0 bis (exklusive) board.m (Zeilen)
+            row = np.random.randint(board.m)
+            col = np.random.randint(board.n)
+
+            if board.fields[row][col] != 0:
+                board.fields[row][col] = self.player_number
+                break
